34ce7082305b7f3e6ea70f02dc170ef904d3a3b4
T1Fleming <eric.fleming101@gmail.com>
2021-01-23 17:40:13 -0800

    save file function

---
+++ mysite/printer/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+import json
+import subprocess
+import base64
+# Create your views here.
+from django.http import HttpResponse
+
+def runCmd(request):
+    process = subprocess.run(['dir'], shell=True ,stdout=subprocess.PIPE, universal_newlines=True)
+    # print(process.stdout)
+
+    # Load File
+    req_str = request.body.decode("utf-8")
+    req_json = json.loads(req_str)
+    req_file_byte = base64.b64decode(req_json["fileToPrint"])
+
+    # Write File
+    open('fileToPrint.txt', 'wb').write(req_file_byte)
+    return HttpResponse('Sent File!')
+
+def index(request):
+    # return HttpResponse("Hit printer index")
+    if(request.path == '/printer/print' and request.method == 'POST'):
+        return runCmd(request)
+    else:
+        return HttpResponse('Humble pie')
+
+    
